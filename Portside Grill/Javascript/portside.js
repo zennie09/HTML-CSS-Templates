63cbd7e5ed612dbc69c45aa5c912980acf85ed4f
zennie09 <zenachristie@gmail.com>
2021-08-11 21:50:44 -0500

    Portside Grill
    This the Portside Grill, which user can use as a menu when they arrive to the restaurant.

---
+++ Portside Grill/Javascript/portside.js
@@ -0,0 +1,61 @@
+/* START DISCOUNT SLIDE */
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("portside_slides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 8000); // Change image every 2 seconds
+}
+/* END DISCOUNT SLIDE */
+
+
+/*START GALLERY SHOW */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+/*END GALLERY SHOW */
+
+
+/* Portside Menu */
+function openNav() {
+  document.getElementById("myNav").style.width = "30%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+/*End Portside Meun */
+
+
+
